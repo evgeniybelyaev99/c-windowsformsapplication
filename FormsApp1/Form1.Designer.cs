334f25da5592a76a27c4e2e1de13d131001c5b20
Evgeniy Belyaev <evgeniy_belyaev_1999@mail.ru>
2019-09-30 19:31:38 +0300

    "Добавьте файлы проекта"

---
+++ FormsApp1/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace FormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.matrix = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.matrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 86);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(297, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сгенерировать матрицу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 34);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(180, 34);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(180, 145);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(63, 22);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(180, 196);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(63, 22);
+            this.textBox4.TabIndex = 6;
+            // 
+            // matrix
+            // 
+            this.matrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrix.Location = new System.Drawing.Point(347, 34);
+            this.matrix.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.matrix.Name = "matrix";
+            this.matrix.Size = new System.Drawing.Size(643, 270);
+            this.matrix.TabIndex = 10;
+            this.matrix.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 145);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 28);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Макс в строке";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 193);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 28);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Мин в столбце";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Строки";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Столбцы";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Location = new System.Drawing.Point(252, 149);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(61, 15);
+            this.textBox5.TabIndex = 15;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Location = new System.Drawing.Point(252, 202);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(61, 15);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(180, 241);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(63, 22);
+            this.textBox9.TabIndex = 19;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(180, 278);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(63, 22);
+            this.textBox10.TabIndex = 20;
+            this.textBox10.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+            // 
+            // textBox11
+            // 
+            this.textBox11.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox11.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox11.Location = new System.Drawing.Point(252, 245);
+            this.textBox11.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(87, 15);
+            this.textBox11.TabIndex = 21;
+            this.textBox11.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
+            // 
+            // textBox12
+            // 
+            this.textBox12.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox12.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox12.Location = new System.Drawing.Point(252, 282);
+            this.textBox12.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(87, 15);
+            this.textBox12.TabIndex = 22;
+            this.textBox12.TextChanged += new System.EventHandler(this.textBox12_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(16, 238);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 28);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Сред в строке";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(16, 276);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 28);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "Пр эл-в столбца";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1020, 382);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.matrix);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.matrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView matrix;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ FormsApp1/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void creatematrix(int m,int n)
+        {
+            matrix.RowCount = m;
+            matrix.ColumnCount = n;
+            Random r = new Random();
+            for (int i = 0; i < m;i++)
+                for (int j = 0; j < n; j++)
+                    matrix.Rows[i].Cells[j].Value = r.Next(10); 
+        }
+        private int max_row(int row)
+        {
+            int max = -1;
+            for (int i=0; i < matrix.ColumnCount; i++)
+            {
+                if ((int)matrix.Rows[row].Cells[i].Value >= max)
+                    max = (int)matrix.Rows[row].Cells[i].Value;
+            }
+            return max;
+        }
+        private int min_column(int column)
+        {
+            int min = 9;
+            for (int i = 0; i < matrix.RowCount; i++)
+            {
+                if ((int)matrix.Rows[i].Cells[column].Value <= min)
+                    min = (int)matrix.Rows[i].Cells[column].Value;
+            }
+            return min;
+        }
+        private double mean_row(int row)
+        {
+            double mean = 0;
+            int count = 0;
+            for (int i = 0; i < matrix.ColumnCount; i++)
+            {
+                mean += (int)matrix.Rows[row].Cells[i].Value;
+                if ((int)matrix.Rows[row].Cells[i].Value == 0)
+                {
+                    count++;
+                }
+            }
+            mean /= (matrix.ColumnCount-count);
+            mean = Math.Round(mean, 1);
+            return mean;
+        }
+        private int multi_column(int column)
+        {
+            int multi = 1;
+            for (int i = 0; i < matrix.RowCount; i++)
+            {
+                
+                    multi *= (int)matrix.Rows[i].Cells[column].Value;
+            }
+            return multi;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int m = int.Parse(textBox1.Text);
+                int n = int.Parse(textBox2.Text);
+                if (m > 0 && n > 0 && m <= 10 && n <= 10)
+                {
+                    creatematrix(m, n);
+                    textBox3.Clear();
+                    textBox4.Clear();
+                    textBox5.Clear();
+                    textBox6.Clear();
+                    textBox9.Clear();
+                    textBox10.Clear();
+                    textBox11.Clear();
+                    textBox12.Clear();
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    button4.Enabled = true;
+                    button5.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Количество строк и столбцов должно изменяться от 1 до 10!");
+                }
+            }
+            catch
+            {
+                
+                if (textBox1.Text == "" || textBox2.Text == "")
+                {
+                    MessageBox.Show("Введите количество строк и столбцов!");
+                }
+                else
+                {
+                    MessageBox.Show("Введены некорректные данные!");
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int row = int.Parse(textBox3.Text);
+                if (row >= 0 && row < (int)matrix.RowCount)
+                {
+                    textBox5.Text = "max= " + max_row(row);
+                }
+                else
+                {
+                    MessageBox.Show("Выбранная строка отсутствует!");
+                    textBox5.Clear();
+                }
+            }
+            catch
+            {
+                if (textBox3.Text == "")
+                {
+                    MessageBox.Show("Введите номер строки!");
+                }
+                else
+                {
+                    MessageBox.Show("Введены некорректные данные!");
+                    textBox5.Clear();
+                }
+            }
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                int column = int.Parse(textBox4.Text);
+                if (column >= 0 && column < (int)matrix.ColumnCount)
+                {
+                    textBox6.Text = "min= " + min_column(column);
+                }
+                else
+                {
+                    MessageBox.Show("Выбранный столбец отсутствует!");
+                    textBox6.Clear();
+                }
+            }
+            catch
+            {
+                if (textBox4.Text == "")
+                {
+                    MessageBox.Show("Введите номер столбца!");
+                }
+                else
+                {
+                    MessageBox.Show("Введены некорректные данные!");
+                    textBox6.Clear();
+                }
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try {
+                int row = int.Parse(textBox9.Text);
+                if (row >= 0 && row < (int)matrix.RowCount)
+                {
+                    textBox11.Text = "mean= " + mean_row(row);
+                }
+                else
+                {
+                    MessageBox.Show("Выбранная строка отсутствует!");
+                    textBox11.Clear();
+                }
+            }
+            catch
+            {
+                if (textBox9.Text == "")
+                {
+                    MessageBox.Show("Введите номер строки!");
+                }
+                else
+                {
+                    MessageBox.Show("Введены некорректные данные!");
+                    textBox11.Clear();
+                }
+            }
+            
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int column = int.Parse(textBox10.Text);
+                if (column >= 0 && column < (int)matrix.ColumnCount)
+                {
+                    textBox12.Text = "multi= " + multi_column(column);
+                }
+                else
+                {
+                    MessageBox.Show("Выбранный столбец отсутствует!");
+                    textBox12.Clear();
+                }
+            }
+            catch
+            {
+                if (textBox10.Text == "")
+                {
+                    MessageBox.Show("Введите номер столбца!");
+                }
+                else
+                {
+                    MessageBox.Show("Введены некорректные данные!");
+                    textBox12.Clear();
+                }
+                
+            }
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
